49000f88cea39345e80708fdc6903ea497149581
arun9617 <62876085+arun9617@users.noreply.github.com>
2021-05-31 22:59:47 +0800

    Add files via upload

---
+++ pq.py
@@ -0,0 +1,32 @@
+x = 4
+while  x < 5:
+    print('Wow you are truely a genius')
+    x = x+1
+
+
+name= ''
+while name !='AishArun':
+    print('Please type you name.')
+    name=input()
+print('Thank You')
+
+
+print('Enter a Number: ')
+n=int(input())
+try:
+    while n<40:
+        print('Number not greater')
+        n=n+1
+except:
+    print('Invalid Input')
+else:
+    print('Thank you')    
+    
+
+
+
+
+
+
+
+
